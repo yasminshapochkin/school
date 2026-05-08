783b4238919c0873dabc39c48657610b375a256a
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-05-08 15:17:44 +0300

    task3 code

---
+++ os/lab02/task3_chain.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+// yungest son finishes first father last!
+int main(int argc, char *argv[] ){
+    if(argc <2){return 1;}
+    int num = atoi(argv[1]);
+    
+    for(int i = 0 ; i < num ; i++){
+
+        pid_t pid = fork();
+        if(pid > 0 ){
+            wait(NULL);
+            break;}
+
+    }
+    
+    printf("Process[%ld],my parent is [%ld]\n", getpid(), getppid());
+
+    return 0;
+
+}
