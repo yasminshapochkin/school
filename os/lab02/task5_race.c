2e478a57d4e045e0799c6c5bc8673a424cfe85d8
Gal <gal052606@gmail.com>
2026-05-08 15:23:26 +0300

    fixed

--- os/lab02/task5_race.c
+++ os/lab02/task5_race.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-int main(int argc,char* argv[]){
-    if(argc<2){
-        return 1;
-    }
-    pid_t pid=-2;
-    int K=atoi(argv[1]);
-    for(int i=0;i<K;i++){
-        pid=fork();
-        if(pid==0){
-            sleep(rand() % 5);
-            printf("child %ld exiting ...\n",getpid());
-            exit(-1);
-        }else{
-            sleep(NULL);
+
+
+// s is sleep, z is zombie  
+
+int main(int argc, char *argv[] ){
+    if(argc < 2){return 1;}
+    int num = atoi(argv[1]);
+    pid_t pid = -2;
+    for(int i = 0 ; i < num ; i++){
+        pid = fork();
+        if(pid == 0 ){
+            sleep(rand()%5); 
+            printf("child %%ld exiting", getpid());
+            exit(0);
         }
     }
-    int status;
     system("ps -l");
-    printf("Parent: Reviewing process state (look for < defunct >)\n");
-    for(int i=0; i<K; i++){
-        if(pid==0)
-            pid=wait(&status);
-        
+    printf("Parent: Reviewing process state (look for <defunc>)", getpid());
+    for(int i = 0 ; i<num ; i++){
+        wait(NULL);
     }
-    printf("Parent: all childrem reaped");
-
+    printf("Parent: all children reaped\n");
     return 0;
 }
