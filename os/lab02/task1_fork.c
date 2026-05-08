bb0fee5a0f02e376f8172fd94b3555fbcb068708
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-05-08 15:11:12 +0300

    task1 code

---
+++ os/lab02/task1_fork.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+/// ex 5 can also do sleep for father , zombe is im done dad isnt , we want son zombie sleep to dad , main goal sons are in zoombie
+
+int main(){
+    int x = 100;
+    pid_t pid = fork();
+    
+    if(pid == 0){
+        x += 50;
+    }
+    else{ wait(NULL);}
+    printf("My PID is %d : x=%d\n", getpid(), X);
+    return 0;
+
