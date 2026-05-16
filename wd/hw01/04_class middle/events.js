c4c05d3cd798a3081959c6d1ba1569b78bc2fbf0
yasmin <yasmin shapochkin@gmail.com>
2026-05-16 15:22:34 +0300

    adding the related files

---
+++ wd/hw01/04_class middle/events.js
@@ -0,0 +1,23 @@
+
+
+let btn1 = document.getElementById("btn1")
+
+
+
+btn1.addEventListener("click", function () {
+    alert("button clicked!")
+  
+});
+
+// when page is Loaded in the browser 
+// what happends is the DOMdocument event if fired.
+document.addEventListener("DOMContentLoaded", function () {
+    PageLoaded();
+  
+});
+
+function PageLoaded(){
+    document.getElementById("title").innerHTML = "HTML is loaded!";
+    alert("page is loaded")
+}
+
---
+++ wd/hw01/04_class middle/index.js
@@ -0,0 +1,20 @@
+
+function navigatePage(btnSelected, url) {
+    //1)  )get all buttons that are in the menu
+    // that have class of menu-btn
+    const buttons =
+        document.querySelectorAll(".menu-btn");
+    // run on all the buttons 
+    buttons.forEach(btn => {
+        //2) remove the selected for eatch
+        btn.classList.remove("selected");
+    });
+    // 3) set selected on the spesific button the function recived 
+    btnSelected.classList.add("selected");
+    // 4) change the sorce url to the one the button 
+
+    // getElementById is kinda equall to -> querySelector("#contentFrame")
+    // it changes the html sorce DOES NOT CHANE TO A NEW PAGE!!!!
+    document.getElementById("contentFrame").src = url;
+}
+
