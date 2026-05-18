06d7a4246c86c64cc1b97917546a36eef69a920c
yasmin <yasmin shapochkin@gmail.com>
2026-05-18 12:30:20 +0300

    fix lab04

--- os/lab04/scheduler_lib.c
+++ os/lab04/scheduler_lib.c
@@ -147,7 +147,7 @@ void simulate_rr(Process proc[], int n, int quantum){
             might_be_ready++;
         }
         //  calc if process needs 2 or 1 time chunks
-        Process_Time_Chunk = proc[actually_running].remaining_time >= 2 ? 2:1;
+        Process_Time_Chunk = proc[actually_running].remaining_time >= quantum ? quantum : proc[actually_running].remaining_time ;
         // decrees from the process remaining work time
         proc[actually_running].remaining_time -=  Process_Time_Chunk;
 
@@ -159,7 +159,7 @@ void simulate_rr(Process proc[], int n, int quantum){
             // update the processes that are finished
             NUMBER_PROCESS_DONE++;
             // update that this one finished
-            proc[actually_running].is_completed =1;
+            proc[actually_running].is_completed = 1;
             // update finish time
             proc[actually_running].finish_time = T ;
             // update the waiting time
