d193817c62a93cdfab7391a2d592781e4e20046b
Gal Elisha <jal425377@gmail.com>
2026-06-02 12:12:01 +0300

    gal lab07

---
+++ os/lab07/Gal/vmem_nru.c
@@ -0,0 +1,112 @@
+#include "vmem_nru.h"
+#include <stdlib.h>
+#include <string.h>
+
+int faults=0;
+int modified=0;
+
+PageTableEntry page_table[NUM_PAGES];
+PageTableEntry *frames[NUM_FRAMES];
+
+void init_frames()
+{
+    for (int i = 0; i < NUM_FRAMES; i++)
+        frames[i] = NULL;
+}
+
+void init_paga_table()
+{
+    for (int i = 0; i < NUM_PAGES; i++)
+    {
+        page_table[i].frame_number = -1;
+        page_table[i].valid = 0;
+    }
+}
+
+int access_memory(uint32_t addr, char op)
+{
+    int vpage = addr / PAGE_SIZE;
+    int offset = addr % PAGE_SIZE;
+    int fr=-1;
+    int vbit = page_table[vpage].valid;
+
+    if (vbit == 1)
+    { // Page Hit
+        page_table[vpage].referenced = 1;
+        if (op == 'W')
+            page_table[vpage].modified = 1;
+    }
+
+    else if (vbit == 0) //Page faults
+    {
+        faults++;
+         NRU(vpage);
+    }
+    printf("Addr: %d | Page: %d | Op: %c | Frame: %d | Status: %s | NRU_Class: %d\n",addr,vpage,op,fr,vbit == 1 ? "HIT" : "FAULT",(page_table[vpage].modified)+(page_table[vpage].referenced * 2));
+
+    return page_table[vpage].frame_number * PAGE_SIZE + offset;
+}
+
+NRU(int pageNum)
+{
+    // free a frame
+    int frame = allocateFreeFrame();
+    if (frames[frame]);
+    {
+        frames[frame]->frame_number = -1;
+        frames[frame]->valid = 0;
+        frames[frame]->referenced = 0;
+        if (frames[frame]->modified == 1)
+        {
+            int pageToEv = frames[frame] - page_table;
+            printf("Evicting dirty page %d", pageToEv);
+            frames[frame]->modified = 0;
+            modified++;
+        }
+    }
+    insertFrame(frame, pageNum);
+    return frame;
+}
+
+void insertFrame(int frameinx, int pageNum)
+{
+    frames[frameinx] = &page_table[pageNum];
+    frames[frameinx]->frame_number = frameinx;
+    frames[frameinx]->valid = 1;
+}
+
+void reset_R_bit(){
+    for (int i = 0; i < NUM_PAGES; i++) {
+        page_table[i].referenced = 0;
+    }
+}
+
+int allocateFreeFrame()
+{
+
+    int R = 0;
+    int M = 0;
+    for (int i = 0; i < NUM_FRAMES; i++)
+    {
+        if (frames[i] == NULL)
+        {
+            return i;
+        }
+        if (frames[i]->modified == M && frames[i]->referenced == R)
+        {
+            return i;
+        }
+        if (i == NUM_FRAMES - 1)
+        {
+            i = -1;
+            if (M == 0)
+                M = 1;
+
+            else if (R == 0)
+            {
+                R = 1;
+                M = 0;
+            }
+        }
+    }
+}
---
+++ os/lab07/Gal/vmem_nru.h
@@ -0,0 +1,25 @@
+#ifndef VMEM_NRU_H
+#define VMEM_NRU_H
+
+#define PAGE_SIZE 4096
+#define NUM_PAGES 16
+#define NUM_FRAMES 4
+#include <stdio.h>
+#include <stdint.h>
+typedef struct PageTableEntry{
+    int frame_number;
+    int valid;
+    int referenced;
+    int modified;
+} PageTableEntry;
+
+void init_frames();
+void init_paga_table();
+
+int access_memory(uint32_t adrr, char op);
+void NRU(int pageNum);
+
+void reset_R_bit()
+void insertFrame(int frameinx, int pageNum);
+int allocateFreeFrame();
+#endif
