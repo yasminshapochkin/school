91e7b5e1476d1a7f3692630af7858fa88d9ceda8
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-06-09 10:22:10 +0300

    lab time

--- os/lab08/yasmin/page_sim.c
+++ os/lab08/yasmin/page_sim.c
@@ -1,4 +1,47 @@
 
 
+// runs sim both ask for frames num while runin 
 
-int run_fifo(int *requests,int num_requests, int frame_count );
+
+
+
+
+// circle array  we make full saize array and move with a pointer
+int run_fifo(int *requests ,int num_requests, int frame_count ){
+    int FRAMES_NUM ;
+    //get number of frames
+    scanf("%d", &FRAMES_NUM);
+    // inisialize data structur
+    FIFO fifo;
+    fifo.size = FRAMES_NUM;
+    fifo.head = 0;
+    fifo.tail = 0;
+    // allocate memory
+    fifo.inserted_order = malloc(sizeof(int) * fifo.size);
+
+    for (int i = 0; i < fifo.size; i++) {
+        fifo.inserted_order[i] = -1;
+    }
+
+    int PAGE_FAULTS_COUNT = 0 ;
+    for(int i = 0 ; i < num_requests ; i++){
+        // add, if not, get a frame out 
+        if(fifo.inserted_order[tail] == -1 ){
+            
+        }
+
+    }
+
+    
+    // free aloocation
+    free(fifo.inserted_order);
+}
+
+
+
+int run_lru(int *requests,int num_requests, int frame_count ){
+    
+
+}
+
+
--- os/lab08/yasmin/page_sim.h
+++ os/lab08/yasmin/page_sim.h
@@ -1,10 +1,51 @@
 
 
-
-
-
 #ifndef PAGE_SIM_H
 #define PAGE_SIM_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
+
+typedef struct {
+
+    int *inserted_order; 
+    int head; 
+    int tail;
+    int size;
+    
+}FIFO;
+
+
+typedef struct {
+
+    int page_num; 
+    int frame_number; 
+    
+    struct node *next;
+    struct node *prev;
+
+}LRU;
+
+
+struct node *head;
+struct node *tail;
+
+
+
+
+
+// runs sim 
+int run_fifo(int *requests,int num_requests, int frame_count );
+
+
+
+int run_lru(int *requests,int num_requests, int frame_count );
+
+
 
 #endif
+
