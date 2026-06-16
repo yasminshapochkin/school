eb44a8f0152af0b076056771cf182d99012d40fc
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-06-16 09:12:39 +0300

    add cdata in files

---
+++ os/lab09/yas/matrix_bench.c
@@ -0,0 +1,7 @@
+void compute_naive(int N, int **A , int **B, int **C){}
+void compute_pthread(int N, int **A , int **B, int **C, int num_threads ){}
+void compute_clone(int N, int **A , int **B, int **C, int num_threads ){}
+
+
+void multiply_worker_pthread(void* arg){}
+void multiply_worker_clone(void* arg){}
