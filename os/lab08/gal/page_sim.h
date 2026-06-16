6fcaeb1b457c119b2099c5692d52d8ba01eb5242
Gal Elisha <jal425377@gmail.com>
2026-06-16 09:02:04 +0300

    lab 8 fifo

---
+++ os/lab08/gal/page_sim.h
@@ -0,0 +1,31 @@
+#ifndef PAGE_SIM_H
+#define PAGE_SIM_H
+
+#define MAX_VPAGE 100
+
+typedef struct FIFOStruct
+{
+    int* frames;
+    int first;
+    int last;
+    int size;
+}FIFOStruct;
+
+typedef struct node_LRU
+{
+    int page_num;
+    int frame_number;
+    struct node_LRU *next;
+    struct node_LRU *prev;
+} node_LRU;
+
+struct node_LRU *head;
+struct node_LRU *tail;
+
+bool in_frames(int* frames,int page, int frame_count);
+
+int run_fifo(int* request, int num_requests, int frame_count);
+
+int run_lru(int* request, int num_requests, int frame_count);
+
+#endif
