3779cc6fe308a34d75a486088584b1fe3e54056b
yasmin <yasmin shapochkin@gmail.com>
2026-06-23 09:05:23 +0300

    write initialize

---
+++ os/lab10/yas/buffer_sync.h
@@ -0,0 +1,14 @@
+
+#ifndef BUFFER_SYNC_H
+#define BUFFER_SYNC_H
+
+
+
+void peterson_init(PetersonLock *lock);
+void peterson_lock(PetersonLock *lock);
+void peterson_unlock(PetersonLock *lock);
+
+void enqueue_task(CircularBuffer *cb , int task_id, int thread_id);
+int dequeue_task(CircularBuffer *cb , int task_id );
+
+#endif
